5f5a52c65bf1732261f979c1fa3f11389acf99ff
qinezh <qezhu@outlook.com>
2015-11-13 11:09:16 +0800

    Update script.cmd

--- script.cmd
+++ script.cmd
@@ -1,7 +1,12 @@
+touch a.txt
+
+eval `ssh-agent -s`
+ssh-add "C:\Users\build\.ssh\id_rsa"
+
 git config user.email "qezhu@outlook.com"
 git config user.name "qinen"
-git add .
-git commit -m "TeamCity auto update"
-git remote -v
-git push -u origin ci
 
+git add .
+git commit -m "TeamCity Auto Update"
+git remote -v
+git push -u origin master
