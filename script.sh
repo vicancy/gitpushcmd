b6b1ec5d117342658b32a298edb337bf63d6fb95
qinezh <qezhu@outlook.com>
2015-11-13 13:24:18 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,12 @@
 eval `ssh-agent -s`
 ssh-add "C:\Users\build\.ssh\id_rsa"
+
+touch a.txt
+git config user.email "qezhu@outlook.com"
+git config user.name "qinen"
+
+git add .
+git commit -m "TeamCity Auto Update"
+git remote -v
+git push -f -u -v origin master
+
